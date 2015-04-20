90ab26ffbd7efb813f5b86763079215928fda3e1
Jim McAndrew <jim@loc8.us>
2015-04-20 10:49:24 -0600

    Update the json to sql tool

--- data/presets/json_to_sql.js
+++ data/presets/json_to_sql.js
@@ -1,70 +1,71 @@
 var fs = require('fs'),
   inputFilename = './presets.json',
   outputFilename = './presets.sql',
-  schemaFilename = './presets.schema.json',
+  schemaFilename = './schema/preset.json',
   tableName = 'table',
   format = function(dataType, data) {
-  var typeArray = dataType.split(' '),
-    types = {
-      'text': function(data) {
-        return '\'' + data.toString() + '\'';
-      },
-      'integer': function(data) {
-        return parseInt(data, 10);
-      },
-      'number': function(data) {
-        return parseFloat(data, 10);
-      },
-      'boolean': function(data) {
-        return JSON.parse(data);
-      },
-      'text[]': function(data) {
-        if (Object.prototype.toString.call(data) === '[object Array]') {
-          var returnValue = '';
-          for (var i = 0; i < data.length; i++) {
-            returnValue += '\'' + data[i].toString() + '\',';
+    var returnValue,
+      types = {
+        'string': function(data) {
+          return '\'' + data.toString() + '\'';
+        },
+        'integer': function(data) {
+          return parseInt(data, 10);
+        },
+        'number': function(data) {
+          return parseFloat(data, 10);
+        },
+        'boolean': function(data) {
+          return JSON.parse(data);
+        },
+        'array': function(data) {
+          if (Object.prototype.toString.call(data) === '[object Array]') {
+            var returnValue = '';
+            for (var i = 0; i < data.length; i++) {
+              returnValue += '\'' + data[i].toString() + '\',';
+            }
+            if (returnValue) {
+              returnValue = returnValue.substring(0, returnValue.length - 1);
+            }
+            return 'ARRAY[' + returnValue + ']';
+          } else {
+            throw 'Array isn\'t really an array';
           }
-          if (returnValue) {
-            returnValue = returnValue.substring(0, returnValue.length - 1);
+        },
+        'object': function(data) {
+          if (Object.prototype.toString.call(data) === '[object Object]') {
+            return '\'' + JSON.stringify(data) + '\'';
+          } else {
+            throw 'Object isn\'t really an object';
           }
-          return 'ARRAY[' + returnValue + ']';
-        } else {
-          throw 'Array isn\'t really an array';
         }
-      },
-      'json': function(data) {
-        if (Object.prototype.toString.call(data) === '[object Object]') {
-          return '\'' + JSON.stringify(data) + '\'';
-        } else {
-          throw 'Object isn\'t really an object';
-        }
+      };
+    for (var type in types) {
+      console.log('x', dataType.type);
+      if (dataType.type === type) {
+        console.log('*', dataType.type);
+        returnValue = types[type](data);
+        break;
       }
-    },
-    returnValue;
-  for (var type in types) {
-    if (typeArray.indexOf(type) >= 0) {
-      returnValue = types[type](data);
-      break;
     }
-  }
-  if (typeArray.indexOf('required') && returnValue === '') {
-    throw 'required field (' + dataType + ') missing';
-  }
-  return returnValue;
-},
+    if (dataType.required && returnValue === '') {
+      throw 'required field (' + dataType + ') missing';
+    }
+    return returnValue;
+  },
   toSql = function() {
     var inserts = [];
     fs.readFile(schemaFilename, 'utf-8', function(err, schema) {
-      schema = JSON.parse(schema);
+      schema = JSON.parse(schema).properties;
       fs.readFile(inputFilename, 'utf-8', function(err, inputData) {
         var columns = [],
           inputJson = JSON.parse(inputData),
-        values = [];
+          values = [];
         for (var key in inputJson) {
           columns = [];
           values = [];
           for (var column in schema) {
-            if (inputJson[key][column] || schema[column].split(' ').indexOf('key') > -1) {
+            if (inputJson[key][column] || schema[column] && typeof schema[column] === 'string' && schema[column].split(' ').indexOf('key') > -1) {
               columns.push(column);
               values.push(format(schema[column], inputJson[key][column] || column));
             }
