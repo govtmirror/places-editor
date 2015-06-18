b9903255d66943473e173f0840db843a00ae7a1b
Jim McAndrew <jim@loc8.us>
2015-06-18 12:25:10 -0600

    Add placeholder icon for items without icons

--- data/presets/build_presets.js
+++ data/presets/build_presets.js
@@ -17,6 +17,7 @@ var csvFilename = 'presets.csv',
 
   csvPath = path.join(__dirname, csvFilename),
   presetPathRoot = path.join(__dirname, presetFoldername),
+  makis = ['maki', 'npmaki'],
 
   parserOptions = {
     //see http://csv.adaltas.com/parse/ for additional parser options,
@@ -94,15 +95,19 @@ var csvFilename = 'presets.csv',
     return list;
   },
   makeIcon = function(preset) {
-    if (preset.Point || preset.Vertex) return preset.JSON_Icon;
-    if (preset.Line) return preset.JSON_Icon_Line;
-    if (preset.Poly) return preset.JSON_Icon_Poly || preset.JSON_Icon_Line;
-    return null;
+    if (makis.indexOf(preset.JSON_Maki) >= 0) {
+      if (preset.Point || preset.Vertex) return preset.JSON_Icon;
+      if (preset.Line) return preset.JSON_Icon_Line;
+      if (preset.Poly) return preset.JSON_Icon_Poly || preset.JSON_Icon_Line;
+      return null;
+    } else {
+      return 'feature-marker-stroked'; // Default
+    }
   },
   makeMaki = function(preset) {
-    if (preset.Point || preset.Vertex) return preset.JSON_Maki;
-    if (preset.Line) return preset.JSON_Maki_Line;
-    if (preset.Poly) return preset.JSON_Maki_Poly || preset.JSON_Maki_Line;
+    if (preset.Point || preset.Vertex) return makis.indexOf(preset.JSON_Maki) >= 0 ? preset.JSON_Maki : null;
+    if (preset.Line) return makis.indexOf(preset.JSON_Maki_Line) >= 0 ? preset.JSON_Maki_Line : null;
+    if (preset.Poly) return makis.indexOf(preset.JSON_Maki_Poly || preset.JSON_Maki_Line) >= 0 ? preset.JSON_Maki_Poly || preset.JSON_Maki_Line : null;
     return null;
   };
 
