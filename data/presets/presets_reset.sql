06cdeec285934acc0246ab14d7cfa8be10645da8
Jim McAndrew <jim@loc8.us>
2015-06-19 16:17:11 -0600

    Update the reset script

--- data/presets/presets_reset.sql
+++ data/presets/presets_reset.sql
@@ -2,28 +2,28 @@
 
 UPDATE nps_render_point
 SET
-  type = o2p_get_type(tags, ARRAY['point'], true),
-  nps_type = o2p_get_type(tags, ARRAY['point'], false),
+  type = o2p_get_name(tags, ARRAY['point'], true),
+  nps_type = o2p_get_name(tags, ARRAY['point'], false),
   rendered = now()
 WHERE
-  type != o2p_get_type(tags, ARRAY['point'], true) OR
-  nps_type != o2p_get_type(tags, ARRAY['point'], false);
+  type != o2p_get_name(tags, ARRAY['point'], true) OR
+  nps_type != o2p_get_name(tags, ARRAY['point'], false);
 
 UPDATE nps_render_line
 SET
-  type = o2p_get_type(tags, ARRAY['line'], true),
-  nps_type = o2p_get_type(tags, ARRAY['line'], false),
+  type = o2p_get_name(tags, ARRAY['line'], true),
+  nps_type = o2p_get_name(tags, ARRAY['line'], false),
   rendered = now()
 WHERE
-  type != o2p_get_type(tags, ARRAY['line'], true) OR
-  nps_type != o2p_get_type(tags, ARRAY['line'], false);
+  type != o2p_get_name(tags, ARRAY['line'], true) OR
+  nps_type != o2p_get_name(tags, ARRAY['line'], false);
 
 UPDATE nps_render_polygon
 SET
-  type = o2p_get_type(tags, ARRAY['area'], true),
-  nps_type = o2p_get_type(tags, ARRAY['area'], false),
+  type = o2p_get_name(tags, ARRAY['area'], true),
+  nps_type = o2p_get_name(tags, ARRAY['area'], false),
   rendered = now()
 WHERE
-  type != o2p_get_type(tags, ARRAY['area'], true) OR
-  nps_type != o2p_get_type(tags, ARRAY['area'], false);
+  type != o2p_get_name(tags, ARRAY['area'], true) OR
+  nps_type != o2p_get_name(tags, ARRAY['area'], false);
 
