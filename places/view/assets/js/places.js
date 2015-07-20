d7ba2b3f9eb436b5e4deb19b06dd69bbf797a6e2
Jim McAndrew <jim@loc8.us>
2015-07-20 15:00:25 -0600

    update the iframes: Update places/view/assets/js/places.js

--- places/view/assets/js/places.js
+++ places/view/assets/js/places.js
@@ -47,8 +47,7 @@ var iframe = document.getElementById('iframe'),
       },
       init: function(callback) {
         var div = document.createElement('div'),
-          sql = 'SELECT full_name FROM parks WHERE the_geom && St_MakePoint({{x}}, {{y}}) AND St_Intersects(the_geom, St_SetSRID(St_MakePoint({{x}}, {{y}}),4326)) ORDER BY area DESC LIMIT 1;',
-
+          sql = 'SELECT * FROM parks WHERE the_geom && St_MakePoint({{x}}, {{y}}) AND St_Intersects(the_geom, St_SetSRID(St_MakePoint({{x}}, {{y}}),4326)) ORDER BY area DESC LIMIT 1;',
           matchOption = function(parkName) {
             var select = document.getElementById('to-park');
             selected = parkName;
