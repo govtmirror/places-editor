d7ed2b18bc03432fdbb5e695ff5c054b0121a821
Nate Irwin <nate@nateirwin.net>
2015-08-06 15:19:08 -0600

    Fix build

--- npmapInstall.js
+++ npmapInstall.js
@@ -2,28 +2,30 @@ console.log('***********************');
 console.log('**       NPMAP       **');
 console.log('***********************');
 
-var fs = require('fs');
 var configFile = __dirname + '/../../config.json';
-var readConfig = require('../../readConfig');
+var fs = require('fs');
 var settingsFile = __dirname + '/js/id/npmap.js';
 var config, idSettings, key;
+
 // Check if our config file exists
 if (fs.existsSync(configFile)) {
+  var readConfig = require('../../readConfig');
+
   config = readConfig(JSON.parse(fs.readFileSync(configFile)));
   idSettings = config.iD;
   key = config.oauth.keys.filter(function (d) {
     return d.name === 'iD';
   })[0];
   idSettings.settings.connection.oauth = {
-    external: config.oauth.external,
-    url: idSettings.settings.connection.api,
     consumerKey: key.consumerKey,
-    secret: key.consumerSecret
+    external: config.oauth.external,
+    secret: key.consumerSecret,
+    url: idSettings.settings.connection.api
   };
+
   fs.writeFileSync(settingsFile, 'iD.npmap = ' + JSON.stringify(idSettings, null, 4) + ';\n');
   console.log('iD.npmap = ' + JSON.stringify(idSettings, null, 4) + ';\n');
   console.log('***********************');
   console.log('NPMap settings updated');
   console.log('***********************');
 }
-
