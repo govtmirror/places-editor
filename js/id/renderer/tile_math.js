2a56e7e24bc548728c1fd0b66148c9bb392d7093
Jim McAndrew <jim@loc8.us>
2015-11-02 13:57:29 -0700

    Reformat and add a bounds function

--- js/id/renderer/tile_math.js
+++ js/id/renderer/tile_math.js
@@ -1,39 +1,43 @@
-iD.tileMath = {
-  //http://alastaira.wordpress.com/2011/07/06/converting-tms-tile-coordinates-to-googlebingosm-tile-coordinates/
-  tile2Tms: function(z, y, x) {
-    var yMax = 1 << z,
-      newY = yMax - y - 1;
+var tileMath = {
+  // http://alastaira.wordpress.com/2011/07/06/converting-tms-tile-coordinates-to-googlebingosm-tile-coordinates/
+  tile2Tms: function (z, y, x) {
+    var yMax = 1 << z;
+    var newY = yMax - y - 1;
     return {
       z: z,
       y: newY,
       x: x
     };
   },
-  lat2tms: function(lat, zoom, pixelOffset) {
+  lat2tms: function (lat, zoom, pixelOffset) {
     var yMax = 1 << zoom;
     return yMax - (module.exports.lat2tile(lat, zoom, pixelOffset)) - 1;
   },
-  //http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-  long2tile: function(lon, zoom, pixelOffset) {
-    return (Math.floor((lon + 180) / 360 * Math.pow(2, zoom) + (pixelOffset ? pixelOffset / 256 : 0)));
+  // http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+  long2tile: function (lon, zoom, pixelOffset, decimal) {
+    var returnValue = ((lon + 180) / 360 * Math.pow(2, zoom) + (pixelOffset ? pixelOffset / 256 : 0));
+    return decimal ? returnValue : Math.floor(returnValue);
   },
-  lat2tile: function(lat, zoom, pixelOffset) {
-    return (Math.floor(((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom)) + (pixelOffset ? pixelOffset / 256 : 0)));
+  lat2tile: function (lat, zoom, pixelOffset, decimal) {
+    var returnValue = (((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom)) + (pixelOffset ? pixelOffset / 256 : 0));
+    return decimal ? returnValue : Math.floor(returnValue);
   },
-  tile2long: function(x, z) {
+  tile2long: function (x, z) {
     return (x / Math.pow(2, z) * 360 - 180);
   },
-  tile2lat: function(y, z) {
+  tile2lat: function (y, z) {
     var n = Math.PI - 2 * Math.PI * y / Math.pow(2, z);
     return (180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n))));
   },
-  //http://www.gal-systems.com/2011/07/convert-coordinates-between-web.html
-  toWgs84: function(mercatorLonX, mercatorLatY) {
-    if (Math.abs(mercatorLonX) < 180 && Math.abs(mercatorLatY) < 90)
+  // http://www.gal-systems.com/2011/07/convert-coordinates-between-web.html
+  toWgs84: function (mercatorLonX, mercatorLatY) {
+    if (Math.abs(mercatorLonX) < 180 && Math.abs(mercatorLatY) < 90) {
       return;
+    }
 
-    if ((Math.abs(mercatorLonX) > 20037508.3427892) || (Math.abs(mercatorLatY) > 20037508.3427892))
+    if ((Math.abs(mercatorLonX) > 20037508.3427892) || (Math.abs(mercatorLatY) > 20037508.3427892)) {
       return;
+    }
 
     var x = mercatorLonX,
       y = mercatorLatY,
@@ -47,8 +51,8 @@ iD.tileMath = {
       'lon': num7 * 57.295779513082323
     };
   },
-  toWebMercator: function(mercatorLonX, mercatorLatY) {
-    if ((Math.abs(mercatorLonX) > 180 || Math.abs(mercatorLatY) > 90))
+  toWebMercator: function (mercatorLonX, mercatorLatY) {
+    if ( (Math.abs(mercatorLonX) > 180 || Math.abs(mercatorLatY) > 90))
       return;
 
     var num = mercatorLonX * 0.017453292519943295,
@@ -60,3 +64,4 @@ iD.tileMath = {
     };
   }
 };
+iD.tileMath = tileMath;
